0731be859cc9a29985f93e504f3c7e3f1a298e32
Roger Wang <rogerwang2520@gmail.com>
2021-10-25 18:48:27 -0500

    Added destroy on click mechanic

---
+++ Assets/Scripts/DestroyOnClick.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+public class DestroyOnClick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Debug.Log("Time survived: " + Time.time + "s");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] int speed;
+    private int xBound = 10;
+    private int yBound = 4;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+        KeepInBounds();
+    }
+
+    void KeepInBounds()
+    {
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+        if (posX > xBound)
+        {
+            posX = xBound;
+        }
+        else if (posX < -xBound)
+        {
+            posX = -xBound;
+        }
+        if (posY > yBound)
+        {
+            posY = yBound;
+        }
+        else if (posY < -yBound)
+        {
+            posY = -yBound;
+        }
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
